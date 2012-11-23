7c37fb3f66a9dc5b9516c8a0c2444e65867d7036
Quarx2k <agent00791@gmail.com>
2012-11-23 14:31:23 +0700

    cleanup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,1 @@
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/full_umts_spyder.mk
-ifeq ($(TARGET_PRODUCT),aokp_umts_spyder)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/aokp.mk
-endif
-ifeq ($(TARGET_PRODUCT),cna_umts_spyder)
-    PRODUCT_MAKEFILES += $(LOCAL_DIR)/cna.mk
-endif
