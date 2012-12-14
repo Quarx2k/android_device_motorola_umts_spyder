9e31c38cfbade6d6659d72321b86aa36136fccb9
Quarx2k <agent00791@gmail.com>
2012-12-15 01:00:45 +0700

    big rootfs cleanup

--- device.mk
+++ device.mk
@@ -23,11 +23,11 @@ PRODUCT_PACKAGES += \
 # Root files
 PRODUCT_COPY_FILES += \
     $(DEVICE_FOLDER)/root/default.prop:/root/default.prop \
-    $(DEVICE_FOLDER)/root/init.mapphone_umts.rc:/root/init.mapphone_umts.rc \
-    $(DEVICE_FOLDER)/root/init.mapphone_umts.rc:/root/init.mapphone_cdma.rc \
-    $(DEVICE_FOLDER)/root/ueventd.rc:/root/ueventd.rc \
-    $(DEVICE_FOLDER)/root/ueventd.mapphone_umts.rc:/root/ueventd.mapphone_umts.rc \
-    $(DEVICE_FOLDER)/root/ueventd.mapphone_umts.rc:/root/ueventd.mapphone_cdma.rc
+    $(DEVICE_FOLDER)/root/init.usb.rc:/root/init.usb.rc \
+    $(DEVICE_FOLDER)/root/init.mapphone.rc:/root/init.mapphone_umts.rc \
+    $(DEVICE_FOLDER)/root/init.mapphone.rc:/root/init.mapphone_cdma.rc \
+    $(DEVICE_FOLDER)/root/ueventd.mapphone.rc:/root/ueventd.mapphone_umts.rc \
+    $(DEVICE_FOLDER)/root/ueventd.mapphone.rc:/root/ueventd.mapphone_cdma.rc \
 
 # Kexec files and ti ducati or rootfs files
 ifeq ($(BOARD_USES_KEXEC),true)
